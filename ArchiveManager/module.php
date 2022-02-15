f33a9c54fcebc7f36727490ce1f14fa396354139
Andreas Seemueller <andreas.seemueller@gmail.com>
2022-02-15 21:34:22 +0100

    changed debug function to route output to messages optionally

--- ArchiveManager/module.php
+++ ArchiveManager/module.php
@@ -147,20 +147,19 @@ class ArchiveManager extends IPSModule {
 
 	}
 	
-	// Version 1.0
+	// Version 1.1
 	protected function LogMessage($message, $severity = 'INFO') {
 		
 		$logMappings = Array();
-		// $logMappings['DEBUG'] 	= 10206; Deactivated the normal debug, because it is not active
-		$logMappings['DEBUG'] 	= 10201;
+		$logMappings['DEBUG'] 	= 10206; 
 		$logMappings['INFO']	= 10201;
 		$logMappings['NOTIFY']	= 10203;
 		$logMappings['WARN'] 	= 10204;
 		$logMappings['CRIT']	= 10205;
 		
-		if ( ($severity == 'DEBUG') && ($this->ReadPropertyBoolean('DebugOutput') == false )) {
+		if ( ($severity == 'DEBUG') && ($this->ReadPropertyBoolean('DebugOutput') == true )) {
 			
-			return;
+			$logMappings['DEBUG'] 	= 10201;
 		}
 		
 		$messageComplete = $severity . " - " . $message;
